c7462b104670ed545e8c27f3e6ba54c2db16c572
SergyNovikLV <mr.noviks@gmail.com>
2020-10-05 12:06:44 +0300

    Initial app files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <Title />
+      <MessageList />
+      <SendMessageForm />
+    </div>
+  )
+}
+
+export default App
